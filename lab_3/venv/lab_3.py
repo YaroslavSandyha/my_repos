8e1c2c0515768132924b99a8ba2aa7a38670dda7
YaroslavSandyha <58649771+YaroslavSandyha@users.noreply.github.com>
2020-11-01 14:50:26 +0200

    lab_3

---
+++ lab_3/venv/lab_3.py
@@ -0,0 +1,86 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+import time
+from sklearn import svm
+from sklearn.decomposition import PCA
+
+def box(output,papka, detector,normType = cv.NORM_L2):
+    bf = cv.BFMatcher(normType)
+    pca = PCA(n_components=1)
+    desi = []
+    support = svm.SVC()
+    training = []
+    test = []
+    with open(papka+"\\training\\training.txt") as file:
+        training = [int(x) for x in file.readline().split()];
+    with open(papka+"\\test\\test.txt") as file:
+        test = [int(x) for x in file.readline().split()];
+    starttime = time.time()
+    for i in range(1, len(training)+1):
+         img2 = cv.imread(papka+"\\training\\{id}.jpg".format(id=i),1)
+         img2 = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
+         kp2, des2 = detector.detectAndCompute(img2, None)
+         X_pca = pca.fit_transform(des2.T).reshape(1, -1)
+         desi.append(X_pca[0])
+    support.fit(desi, training)
+    desi = []
+    for i in range( 1, len(test) + 1):
+        img2 = cv.imread(papka + "\\test\\{id}.jpg".format(id=i), 1)
+        img2 = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
+        kp2, des2 = detector.detectAndCompute(img2, None)
+        X_pca = pca.fit_transform(des2.T).reshape(1, -1)
+        desi.append(X_pca[0])
+    pred = support.predict(desi)
+    endtime = time.time()
+    test_true_realy1 = 0
+    test_true1 = 0
+    test_true0 = 0
+    test_true_realy0 = 0
+    for i in range(0,len(test)):
+        if test[i] == 0:
+            if test[i] != pred[i]:
+                test_true0+=1
+            test_true_realy0 +=1
+        else:
+            if test[i] != pred[i]:
+                test_true1+=1
+            test_true_realy1 +=1
+    print(output)
+    print("false negative",test_true0,'\\',test_true_realy0)
+    print("false positive",test_true1, '\\', test_true_realy1)
+    print(test_true0 + test_true1, '\\', test_true_realy0+test_true_realy1)
+    print("Time: ",endtime - starttime)
+
+    cap = cv.VideoCapture(papka + '\\video2.mp4')
+    img1 = cv.imread(papka + "\\training\\1.jpg", 1)
+    des1 = detector.detectAndCompute(img1, None)
+    while (True):
+        B = []
+        ret, frame = cap.read()
+        dim = (512, 756)
+        frame2 = cv.resize(frame, dim, interpolation=cv.INTER_AREA)
+        try:
+            kp2, des2 = detector.detectAndCompute(frame2, None)
+            desq = pca.fit_transform(des2.T).reshape(1, -1)
+            B.append(desq[0])
+            y = support.predict(B)
+            if y[0] == 1:
+                font = cv.FONT_HERSHEY_SIMPLEX
+                cv.putText(frame2,
+                           'Na meste',
+                           (50, 50),
+                           font, 1,
+                           (0, 255, 255),
+                           2,
+                           cv.LINE_4)
+        except:
+            pass
+        cv.imshow('frame', frame2)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv.destroyAllWindows()
+
+
+box("AKAZE","Kharchenko_A",cv.AKAZE_create(),normType = cv.NORM_L2)
--- untitled5/venv/lab_2.py
+++ untitled5/venv/lab_2.py
@@ -2,54 +2,78 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 import time
+from sklearn import svm
+from sklearn.decomposition import PCA
+#from sklearn.cross_validation import train_test_split
+# def ORB():
+#     training = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+#                  1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1,
+#                  1, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0,
+#                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#     return training
+# def SIFT():
+#     training= [1,0,1,1,1,1,0,0,0,0,
+#                 1,1,0,1,1,1,1,1,0,0,
+#                 1,1,1,0,1,0,0,0,0,0,
+#                 0,0,0,0,1,0,0,0,0,0,
+#                 0,0,0,1,1,1,0,0,0,1,
+#                 0,1,1,1,1,1,1,1,1,1,
+#                 1,1,0,1,1,1,0,1,1,1,
+#                 1,1,0,0,0,1,0,0,0,1]
+#     return training
+# def AKAZE():
+#     training = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+#                 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+#                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0,
+#                 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1,
+#                 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+#                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+#     return training
+# def box(output,papka,papka2, detector,painted = False,normType = cv.NORM_L2):
+#     f = open(output+'\\1_true_{q}.txt'.format(q=papka), 'w')
+#     f2 = open(output+'\\1_localization_{q}.txt'.format(q=papka), 'w')
+#     f3 = open(output+'\\1_time_{q}.txt'.format(q=papka), 'w')
+#     f4 = open(output+'\\2_true_{q}.txt'.format(q=papka), 'w')
+#     f5 = open(output+'\\2_localization_{q}.txt'.format(q=papka), 'w')
+#     f6 = open(output+'\\2_time_{q}.txt'.format(q=papka), 'w')
+#     img1 = cv.imread(papka+'\\true.jpg', 1)
+#     img1 = cv.cvtColor(img1, cv.COLOR_BGR2RGB)
+#     kp1, des1 = detector.detectAndCompute(img1, None)
+#     bf = cv.BFMatcher(normType)
+#     norm = len(bf.knnMatch(des1, des1, k=2))
+#     pca = PCA(n_components=1)
+#     desi =[]
+#     support = svm.SVC()
+#     for i in range (1, 47):
+#         start_time  = time.time()
+#         img2 = cv.imread(papka2+"\\{id}.jpg".format(id=i),1)
+#         img2 = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
+#
+#         kp2, des2 = detector.detectAndCompute(img2, None)
+#         X_pca = pca.fit_transform(des2.T).reshape(1, -1)
+#         desi.append(X_pca[0])
+#         print (i)
+#     support.fit(desi, training)
+#     pred = support.predict(desi)
+#     print (pred)
+#     desi = []
+#     for i in range ( 40, 121):
+#         start_time = time.time()
+#         img2 = cv.imread(papka + "\\{id}.jpg".format(id=i), 1)
+#         img2 = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
+#         kp2, des2 = detector.detectAndCompute(img2, None)
+#         X_pca = pca.fit_transform(des2.T).reshape(1, -1)
+#         desi.append(X_pca[0])
+#         print(i)
+#     pred = support.predict(desi)
+#     print(pred)
+#
+# box("AKAZE","Sandyha_Y","testing",cv.ORB_create(), painted = False,normType = cv.NORM_HAMMING)
+cap = cv.VideoCapture(0)
+ret, frame = cap.read()
+dim = (512, 756)
+frame2 = cv.resize(frame, dim, interpolation=cv.INTER_AREA)
+cv.imshow('frame', frame2)
 
-
-def box(output,papka,detector,painted = False,normType = cv.NORM_L2):
-    f = open(output+'\\1_true_{q}.txt'.format(q=papka), 'w')
-    f2 = open(output+'\\1_localization_{q}.txt'.format(q=papka), 'w')
-    f3 = open(output+'\\1_time_{q}.txt'.format(q=papka), 'w')
-    f4 = open(output+'\\2_true_{q}.txt'.format(q=papka), 'w')
-    f5 = open(output+'\\2_localization_{q}.txt'.format(q=papka), 'w')
-    f6 = open(output+'\\2_time_{q}.txt'.format(q=papka), 'w')
-    img1 = cv.imread(papka+'\\true.jpg', 1)
-    img1 = cv.cvtColor(img1, cv.COLOR_BGR2RGB)
-    kp1, des1 = detector.detectAndCompute(img1, None)
-    bf = cv.BFMatcher(normType)
-    norm = len(bf.knnMatch(des1, des1, k=2))
-    for i in range (1, 121):
-        start_time  = time.time()
-        img2 = cv.imread(papka+"\\{id}.jpg".format(id=i),1)
-        img2 = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
-
-        kp2, des2 = detector.detectAndCompute(img2, None)
-        good_point_two = []
-        good_point = []
-        matches = bf.knnMatch(des1,des2, k=2)
-        if (len(matches[0])!=1):
-            for m, n in matches:
-                if m.distance < 0.89* n.distance:
-                    good_point.append([m])
-                    good_point_two.append([(pow(m.distance,1/2))])
-            if(i<=100):
-                f.write('img' + str(i) + ' ' + str(len(good_point))+' / '+ str(norm) + '\n')
-                f2.write('img' + str(i) + ' ' + str( np.mean(good_point_two)) + '\n')
-                f3.write('img' + str(i) + "--- %s seconds ---" % (time.time() - start_time) + '\n')
-
-            elif(i>100):
-                f4.write('img' + str(i) + ' ' + str(len(good_point))+ '/'+ str(norm) + '\n')
-                f5.write('img' + str(i) + ' ' + str(np.mean(good_point_two)) + '\n')
-                f6.write('img' + str(i) + "--- %s seconds ---" % (time.time() - start_time) + '\n')
-            if painted:
-                img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good_point, None, flags=2)
-                plt.imshow(img3),plt.show()
-        else:
-            f.write('img' + str(i) + ' no matches' +'\n')
-            f2.write('img' + str(i) + ' no matches' + '\n')
-            f3.write('img' + str(i) + ' no matches' +'\n')
-            f4.write('img' + str(i) + ' no matches' + '\n')
-            f5.write('img' + str(i) + ' no matches' +'\n')
-            f6.write('img' + str(i) + ' no matches' +'\n')
-        print(i)
-
-
-box("ORB","Kharchenko_A",cv.ORB_create(), painted = False,normType = cv.NORM_HAMMING)
+#cap.release()
+#cv.destroyAllWindows()
